65a099a29dbdc57d35ee31202db3ce3f3ed41635
Harry San Juan <harrycsanjuan@gmail.com>
2024-10-07 22:38:08 -0400

    add item post resource

--- server/app.py
+++ server/app.py
@@ -29,6 +29,40 @@ class Items(Resource):
             return response
         else:
             return {'error': 'Unexpected Server Error'}, 500
+        
+    def post(self):
+        json = request.get_json()
+
+        try:
+            new_item = Item(
+            item_name=json['item_name'],
+            category=json['category'],
+            stock_quantity=json['stock_quantity'],
+            reorder_quantity=json['reorder_quantity'],
+        )
+            db.session.add(new_item)
+            db.session.commit()
+
+        except ValueError as e:
+            return {'errors': str(e)}, 400
+        
+        except Exception as e:
+            return {'errors': 'Failed to add item to database', 'message': str(e)}, 500
+        
+        item_in_db = Item.query.filter(
+            Item.item_name == json['item_name'],
+            Item.category == json['category'],
+            Item.stock_quantity == json['stock_quantity'],
+            Item.reorder_quantity == json['reorder_quantity']
+        ).first()
+
+        item_dict = item_in_db.to_dict(rules=('-restock_orders',))
+
+        if item_dict:
+            try:
+                return item_dict, 200
+            except Exception as e:
+                return {'errors': 'Item not found'}, 400
 
 api.add_resource(Items, '/items', endpoint='/items')
 
